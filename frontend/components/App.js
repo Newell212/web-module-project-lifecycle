83af77db772ef9b1c4f645b5a18aa4d7e4dc1d41
Michael Newell <michael.newell212@gmail.com>
2023-03-27 20:58:47 -0600

    added state for input

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,7 +6,12 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state = {
     todos: [],
-    error: ''
+    error: '',
+    todoNameInput: '',
+  }
+  onTodoNameInputChange = evt => {
+    const {value} = evt.target
+    this.setState({...this.state, todoNameInput: value})
   }
   fetchAllTodos = () => {
     axios.get(URL)
@@ -33,7 +38,7 @@ export default class App extends React.Component {
           }
         </div>
         <form id="todoForm">
-        <input type="text" placeholder="Type todo"></input>
+        <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type todo"></input>
         <input type="submit"></input>
         <button>Clear Completed</button>
         </form>
