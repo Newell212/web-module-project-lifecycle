e689b83835f3f89430841c3ba823018601d8f41a
Michael Newell <michael.newell212@gmail.com>
2023-03-27 22:13:57 -0600

    added Todo component

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -3,16 +3,7 @@ import React from 'react'
 export default class Todo extends React.Component {
   render() {
     return (
-      <div id="todos">
-          <h2>Todos:</h2>
-          {
-            this.props.todos.reduce((acc, td) => {
-              if (this.props.displayCompleteds || !td.completed) return acc.concat(<div onClick={this.props.toggleCompleted(td.id)} key={td.id}>{td.name}{td.completed ? ' ✔️' : ''}</div>)
-              return acc
-            }, [])
-           
-          }
-        </div>
+      <div onClick={this.props.toggleCompleted(this.props.todo.id)} >{this.props.todo.name}{this.props.todo.completed ? ' ✔️' : ''}</div>
     )
   }
 }
